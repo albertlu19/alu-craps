fedf4c1d64bbfae1a94b259d11dff47d465aa604
Albert Lu <alu19@students.polytechnic.org>
2019-01-23 23:02:36 -0800

    added Die/Dice classes

---
+++ Dice.java
@@ -0,0 +1,28 @@
+
+/**
+ * Write a description of class Dice here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Dice
+{
+    private Die d1;
+    private Die d2;
+    /**
+     * Constructor for objects of class Dice
+     */
+    public Dice()
+    {
+        d1 = new Die();
+        d2 = new Die();
+    }
+
+    /**
+     * rolls out an int array which contains the dice
+     */
+    public int[] roll()
+    {
+        
+    }
+}
---
+++ Die.java
@@ -0,0 +1,24 @@
+
+/**
+ * This class creates a die that can roll.
+ *
+ * Albert Lu
+ * 1/23/18
+ */
+public class Die
+{
+    /**
+     * constructor for the Die class
+     */
+    public Die()
+    {
+        // initialise instance variables
+
+    }
+    
+    public int roll(){
+        int random = (int)(Math.random()*6+1);
+        return random;
+    }
+
+}
